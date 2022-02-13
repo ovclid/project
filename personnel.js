a2981e9f6e9c526bff8276f2fff1f6417a5b20cd
ovclid <53159064+ovclid@users.noreply.github.com>
2022-02-13 20:42:08 +0900

    Add files via upload

--- personnel.js
+++ personnel.js
@@ -1,7 +1,7 @@
 var info = [
-    { name: "나", birth_year: 1978, birth_month: 2, birth_day: 10 },
-    { name: "아내", birth_year: 1982, birth_month: 10, birth_day: 03 },
-    { name: "아들", birth_year: 2011, birth_month: 11, birth_day: 17 },
+    { name: "나", bir_party : "음력", birth_year: 1978, birth_month: 2, birth_day: 10, lunar_year: 1978, lunar_month: 1, lunar_day: 4},
+    { name: "아내", bir_party : "양력", birth_year: 1982, birth_month: 10, birth_day: 03, lunar_year: 1982, lunar_month: 8, lunar_day: 17 },
+    { name: "아들", bir_party : "양력", birth_year: 2011, birth_month: 11, birth_day: 17, lunar_year: 2011, lunar_month: 10, lunar_day: 22 },
 
 ];
 
@@ -26,23 +26,34 @@ function cal_real_age(year, month, day) {
     return age;
 }
 
+function birthday_party(){
+
+}
 function draw_table() {
     const rowCnt = info.length;
     const columnCnt = 4;
 
     document.write('<table border="1">');
-    document.write('<th>');            
-    document.write('<td> 성명 </td>');
-    document.write('<td> 생년월일 </td>');
-    document.write('<td> 나이 </td>');
-    document.write('<td> 만나이 </td>');
-    document.write('</th>');
+    document.write('<th> 순번 </th>');
+    document.write('<th> 성명 </th>');
+    document.write('<th> 생파 </th>');
+    document.write('<th> 생년월일 </th>');
+    document.write('<th> 음력생일 </th>');
+    document.write('<th> 나이 </th>');
+    document.write('<th> 만(세) </th>');
 
     for (let i = 0; i < rowCnt; i++) {
-        document.write('<tr>');
+        if ( i%2 == 0) {
+            document.write('<tr>');
+        }
+        else{
+            document.write("<tr style = 'background-color: lightcyan;'>");
+        }
         document.write('<td>', i+1, "&nbsp", '</td>');
         document.write('<td>', info[i].name, "&nbsp", '</td>');
+        document.write('<td>', info[i].bir_party, "&nbsp", '</td>');
         document.write('<td>', info[i].birth_year, ".&nbsp", info[i].birth_month, ".&nbsp", info[i].birth_day, ".&nbsp", "</td>");
+        document.write('<td>', info[i].lunar_year, ".&nbsp", info[i].lunar_month, ".&nbsp", info[i].lunar_day, ".&nbsp", "</td>");
         document.write('<td>', cal_age(info[i].birth_year, info[i].birth_month, info[i].birth_day), '</td>');
         document.write('<td>', cal_real_age(info[i].birth_year, info[i].birth_month, info[i].birth_day), "</td>");
         document.write('</tr>')
